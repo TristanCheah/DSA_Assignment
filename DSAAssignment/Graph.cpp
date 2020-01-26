6442cc2fb03d937474e7612e3e7ad87ac02ba91c
Cjeng Jacob Lee <s10177571@connect.np.edu.sg>
2020-01-26 20:55:07 +0800

    split it so that stations and names listed seperately

--- DSAAssignment/Graph.cpp
+++ DSAAssignment/Graph.cpp
@@ -26,19 +26,28 @@ string Graph::readCSV(string file_name) {
 	file.open("Stations.csv");
 	if (file.is_open()) {
 		string word;
+		string stations;
+		string names;
 		string output;
 
 		while (!file.eof()) {
 			string row;
 			getline(file, row);
 			std::stringstream s_stream(row);
-			
-
+			int count = 0;
 			while (getline(s_stream, word, ',')) {
-				output += word;
+				if (count == 0) {
+					stations += word + "\n";
+					count++;
+				}
+				else {
+					names += word + "\n";
+				}
 			}
-			output += "\n";
 		}
+		stations += "\n";
+		names += "\n";
+		output = stations + names;
 		return output;
 	}
 	return "";
