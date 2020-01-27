c4fa80c917ae9c3e1f17d4495a9aa0ccf2a9fcce
Cjeng Jacob Lee <s10177571@connect.np.edu.sg>
2020-01-28 01:46:01 +0800

    added hash tables to store each line and stations to find by line

--- DSAAssignment/Graph.cpp
+++ DSAAssignment/Graph.cpp
@@ -1,18 +1,27 @@
 #include "pch.h"
 #include "Graph.h"
+#include <iostream>
 #include <fstream>
 #include <string>
 #include <sstream>
+
 using namespace std;
 
-
-Graph::Graph(Node* start, Node* end)
+int charvalue(char c)
 {
-	startNode = start;
-	endNode = end;
+	if (isalpha(c))
+	{
+		return (int)toupper(c) - (int) 'A';
+	}
+	else
+		return 0;
 }
-Graph::Graph() {
 
+Graph::Graph() {
+	for (int i = 0; i < MAX_SIZE; i++) {
+		items[i] = NULL;
+	}
+	size = 0;
 }
 
 Graph::~Graph()
@@ -21,34 +30,103 @@ Graph::~Graph()
 void Graph::generate_graph() {
 	
 }
-string Graph::readCSV(string file_name) {
+
+int Graph::hash(KeyType key) {
+	int hash = 0;
+	for (int i = 0; i < key.length(); i++) {
+		hash += charvalue(key[i]);
+	}
+	hash = hash % MAX_SIZE;
+	return hash;
+}
+
+bool Graph::add(KeyType newKey, ItemType newItem) {
+	int hash = this->hash(newKey);
+	Node *n = new Node;
+	n->key = newKey;
+	n->item = newItem;
+	n->next = NULL;
+	if (items[hash] == NULL) {
+		items[hash] = n;
+		size += 1;
+		return true;
+	}
+	else {
+		Node *current = items[hash];
+		if (current->key == n->key) {
+			//cout << "duplicate found" << endl;
+			return false;
+		}
+		while (current->next != NULL) {
+			if (current->key == n->key) {
+				//cout << "duplicate found" << endl;
+				return false;
+			}
+			current = current->next;
+		}
+		current->next = n;
+		size += 1;
+		//cout << "entered after traversing" << endl;
+		//cout << to_string(hash) << endl;
+		return true;
+	}
+}
+
+string Graph::getLine(KeyType key) {
+	string no;
+	string name;
+	string output;
+	int hash = this->hash(key);
+	if (items[hash] != NULL) {
+		Node* current = items[hash];
+		while (current != NULL) {
+			no += current->key + "\n";
+			name += current->item + "\n";
+			current = current->next;
+		}
+		no += "\n";
+		output = no + name;
+	}
+	return output;
+}
+
+
+void Graph::readCSV(string file_name) {
 	ifstream file;
 	file.open("Stations.csv");
 	if (file.is_open()) {
 		string word;
-		string stations;
-		string names;
-		string output;
-
 		while (!file.eof()) {
+			string no;
+			string Name;
 			string row;
 			getline(file, row);
 			std::stringstream s_stream(row);
 			int count = 0;
 			while (getline(s_stream, word, ',')) {
 				if (count == 0) {
-					stations += word + "\n";
+					no = word;
 					count++;
 				}
 				else {
-					names += word + "\n";
+					Name = word;
+					this->add(no, Name);
 				}
 			}
 		}
-		stations += "\n";
-		names += "\n";
-		output = stations + names;
-		return output;
 	}
-	return "";
+}
+
+void Graph::print() {
+	for (int i = 0; i < MAX_SIZE; i++) {
+		Node* current = new Node;
+		if (items[i] != NULL) {
+			current = items[i];
+			while (current != NULL) {
+				cout << "Number : " << current->key << endl;
+				cout << "Name : " << current->item << endl;
+				current = current->next;
+			}
+		}
+	}
 }
--- DSAAssignment/Graph.h
+++ DSAAssignment/Graph.h
@@ -2,26 +2,29 @@
 using namespace std;
 #include <string>
 
+const int MAX_SIZE = 11;
 typedef string ItemType;
+typedef string KeyType;
+
 class Graph
 {
 private:
 	struct Node {
 		ItemType item;
+		KeyType key;
 		Node* next;
-		float distance_to_next;
 	};
-	Node* startNode;
-	Node* endNode;
+	Node *items[MAX_SIZE];
+	int size;
+
 public:
-	Graph(Node* start, Node* end);
 	Graph();
 	~Graph();
 	void generate_graph();
-	string readCSV(string filename);
-	
-	
-
-
+	void readCSV(string filename);
+	int hash(KeyType key);
+	bool add(KeyType newKey, ItemType newItem);
+	string getLine(KeyType key);
+	void print();
 };
 
